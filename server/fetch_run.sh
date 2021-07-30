932a389d7e22e65a5c85d9e0171961e59dd33912
Eile Kerning <tysayahi@gmail.com>
2021-07-30 01:42:21 -0400

    Update fetch_run.sh

--- server/fetch_run.sh
+++ server/fetch_run.sh
@@ -6,6 +6,8 @@ echo "Update Start"
     git pull origin master
 echo "Update Complete"
 cd ..
+rm -rf ./output/ || true
+mkdir output
 pwd
 #echo "Export Variables"
 #    export GIT_TRACE_PACKET=1
@@ -15,3 +17,7 @@ pwd
 #    chmod +x ./build-scripts/scripts/clean.sh
 #    time bash ./build-scripts/scripts/clean.sh
 echo "Kernel Start"
+chmod +x ./build-scripts/scripts/kernel-6700k.sh || true
+./build-scripts/scripts/kernel-6700k.sh >> ./log/6700k
+chmod +x ./build-scripts/scripts/kernel-4700u.sh || true
+./build-scripts/scripts/kernel-4700u.sh >> ./log/4700u
