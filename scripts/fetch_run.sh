bd35d00c33bd835bda3a5805ab36ea677fe83aa4
Eile Kerning <tysayahi@gmail.com>
2021-07-31 18:02:56 -0400

    Fix file execute permission

--- scripts/fetch_run.sh
+++ scripts/fetch_run.sh
@@ -16,14 +16,14 @@ echo "Kernel Start"
 mkdir -p ${BWDIR}/log/ || true
 touch ${BWDIR}/log/6700k || true
 touch ${BWDIR}/log/4700u || true
-chmod +x ${BWDIR}/build-scripts/scripts/kernel-6700k.sh || true
+#chmod +x ${BWDIR}/build-scripts/scripts/kernel-6700k.sh || true
 ${BWDIR}/build-scripts/scripts/kernel-6700k.sh >> ${BWDIR}/log/6700k
-chmod +x ${BWDIR}/build-scripts/scripts/kernel-4700u.sh || true
+#chmod +x ${BWDIR}/build-scripts/scripts/kernel-4700u.sh || true
 ${BWDIR}/build-scripts/scripts/kernel-4700u.sh >> ${BWDIR}/log/4700u
 echo "Kernel Complete"
 echo "Nvidia Start"
 cd ${BWDIR}
-chmod +x ${BWDIR}/build-scripts/scripts/nvidia.sh || true
+#chmod +x ${BWDIR}/build-scripts/scripts/nvidia.sh || true
 touch ${BWDIR}/log/nvidia || true
 ./${BWDIR}/build-scripts/nvidia.sh >> ${BWDIR}/log/nvidia
 echo "Nvidia Complete"
@@ -31,5 +31,5 @@ cd ${BWDIR}
 cp ${BWDIR}/output/*.pkgs.tar.zst ~/packages/
 echo "Packages Start"
 touch ${BWDIR}/log/packages || true
-chmod +x ${BWDIR}/build-scripts/scripts/packages.sh || true
+#chmod +x ${BWDIR}/build-scripts/scripts/packages.sh || true
 ./${BWDIR}/build-scripts/scripts/packages.sh >> ${BWDIR}/log/packages
