39ec210b04ca8d8367ec824ad50b22b4aebc8890
Eile Kerning <tysayahi@gmail.com>
2021-07-31 11:07:50 -0400

    Update fetch_run.sh

--- scripts/fetch_run.sh
+++ scripts/fetch_run.sh
@@ -28,6 +28,7 @@ touch ${BWDIR}/log/nvidia || true
 ./${BWDIR}/build-scripts/nvidia.sh >> ${BWDIR}/log/nvidia
 echo "Nvidia Complete"
 cd ${BWDIR}
+cp ${BWDIR}/output/*.pkgs.tar.zst ~/packages/
 echo "Packages Start"
 touch ${BWDIR}/log/packages || true
 chmod +x ${BWDIR}/build-scripts/scripts/packages.sh || true
