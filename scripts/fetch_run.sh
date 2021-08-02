8a85a3c1b92ab1873deaf3da7b95677993ab29b2
Eile Kerning <tysayahi@gmail.com>
2021-08-02 14:45:27 -0400

    Update fetch_run.sh

--- scripts/fetch_run.sh
+++ scripts/fetch_run.sh
@@ -17,9 +17,9 @@ mkdir -p ${BWDIR}/log/ || true
 touch ${BWDIR}/log/6700k || true
 touch ${BWDIR}/log/4700u || true
 #chmod +x ${BWDIR}/build-scripts/scripts/kernel-6700k.sh || true
-${BWDIR}/build-scripts/scripts/kernel-6700k.sh >> ${BWDIR}/log/6700k
+#${BWDIR}/build-scripts/scripts/kernel-6700k.sh >> ${BWDIR}/log/6700k
 #chmod +x ${BWDIR}/build-scripts/scripts/kernel-4700u.sh || true
-${BWDIR}/build-scripts/scripts/kernel-4700u.sh >> ${BWDIR}/log/4700u
+#${BWDIR}/build-scripts/scripts/kernel-4700u.sh >> ${BWDIR}/log/4700u
 echo "Kernel Complete"
 echo "Nvidia Start"
 cd ${BWDIR}
