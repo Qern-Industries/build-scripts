10f52e5ee53b63c256c6696a312fb52c73e35a17
Eile Kerning <tysayahi@gmail.com>
2021-08-02 15:23:13 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,16 +4,10 @@ mkdir output
 pwd
 echo "Kernel Start"
 mkdir -p ${BWDIR}/log/ || true
-touch ${BWDIR}/log/6700k || true
-touch ${BWDIR}/log/4700u || true
-#chmod +x ${BWDIR}/build-scripts/scripts/kernel-6700k.sh || true
-#${BWDIR}/build-scripts/scripts/kernel-6700k.sh >> ${BWDIR}/log/6700k
-#chmod +x ${BWDIR}/build-scripts/scripts/kernel-4700u.sh || true
-#${BWDIR}/build-scripts/scripts/kernel-4700u.sh >> ${BWDIR}/log/4700u
+#${BWDIR}/build-scripts/scripts/kernel.sh
 echo "Kernel Complete"
 echo "Nvidia Start"
 cd ${BWDIR}
-#chmod +x ${BWDIR}/build-scripts/scripts/nvidia.sh || true
 touch ${BWDIR}/log/nvidia || true
 ${BWDIR}/build-scripts/scripts/nvidia.sh >> ${BWDIR}/log/nvidia
 echo "Nvidia Complete"
@@ -21,6 +15,5 @@ cd ${BWDIR}
 cp ${BWDIR}/output/*.pkg.tar.zst ~/packages/
 echo "Packages Start"
 touch ${BWDIR}/log/packages || true
-#chmod +x ${BWDIR}/build-scripts/scripts/packages.sh || true
 ${BWDIR}/build-scripts/scripts/packages.sh >> ${BWDIR}/log/packages
 echo "Think this works."
