05b2931e2c372443896b2e8bb65f3a0b2e70c33d
Eile Kerning <tysayahi@gmail.com>
2021-08-02 14:56:53 -0400

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,8 +1,5 @@
 set -x
 cd ${BWDIR}
-rm -rf ${BWDIR}/work || true
-mkdir ${BWDIR}/work
-cd ${BWDIR}/work
 list_unsorted="$(cat ${BWDIR}/build-scripts/list)"
 list="${list_unsorted//$'\n'/ }"
 echo $list
