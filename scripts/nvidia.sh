c03e379cd18c3c000e0cd916a7510223aa602091
Eile Kerning <tysayahi@gmail.com>
2021-08-02 14:59:51 -0400

    Update nvidia.sh

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -6,6 +6,7 @@ cd ${BWDIR}/work
 git clone --depth 1 https://github.com/Frogging-Family/nvidia-all
 rm ${BWDIR}/work/nvidia-all/customization.cfg
 cp ${BWDIR}/build-scripts/cfg/nvidia-5.13.cfg ${BWDIR}/work/nvidia-all
+cd ${BWDIR}/work/nvidia-all
 makepkg
 cd ${BWDIR}
 cp ${BWDIR}/work/nvidia-all/*.pkg.tar.zst ${BWDIR}/output
