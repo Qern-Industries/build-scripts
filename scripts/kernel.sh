7a955f931cf88ce62a0ef01a8ec5ad6f09789ac8
Eile Kerning <tysayahi@gmail.com>
2021-08-25 18:45:27 -0400

    Update kernel.sh

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -8,6 +8,7 @@ cd ${BWDIR}/reuse/git
 git clone https://github.com/Frogging-Family/linux-tkg || true
 sed -i "s|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/desktop-6700k.cfg|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|g" ${BWDIR}/reuse/git/linux-tkg/customization.cfg || true
 sed -i "s|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/zenbook-14-4700u.cfg|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|g" ${BWDIR}/reuse/git/linux-tkg/customization.cfg || true
+cd ${BWDIR}/reuse/git/linux-tkg
 git pull origin || true
 rm ${BWDIR}/reuse/cfg/kernel/* || true
 cp ${BWDIR}/build-scripts/cfg/zenbook-14-4700u.cfg ${BWDIR}/reuse/cfg/kernel/
