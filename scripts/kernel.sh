6cc196985dcd1ee07f131368cf9efbf018cf9a9f
Eile Kerning <tysayahi@gmail.com>
2021-08-27 14:14:29 -0400

    Update kernel.sh

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -6,21 +6,25 @@ mkdir -p ${BWDIR}/reuse/git || true
 mkdir -p ${BWDIR}/reuse/cfg/kernel || true
 cd ${BWDIR}/reuse/git
 git clone https://github.com/Frogging-Family/linux-tkg || true
-sed -i "s|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/desktop-6700k.cfg|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|g" ${BWDIR}/reuse/git/linux-tkg/customization.cfg || true
-sed -i "s|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/zenbook-14-4700u.cfg|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|g" ${BWDIR}/reuse/git/linux-tkg/customization.cfg || true
 cd ${BWDIR}/reuse/git/linux-tkg
 git pull origin || true
 rm ${BWDIR}/reuse/cfg/kernel/* || true
 cp ${BWDIR}/build-scripts/cfg/zenbook-14-4700u.cfg ${BWDIR}/reuse/cfg/kernel/
 cp ${BWDIR}/build-scripts/cfg/desktop-6700k.cfg ${BWDIR}/reuse/cfg/kernel/
+
+cd ${BWDIR}/reuse/git/linux-tkg
+git fetch --all
+git reset --hard origin/master
 sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/desktop-6700k.cfg|g" ${BWDIR}/reuse/git/linux-tkg/customization.cfg || true
+makepkg -s 
+
+cd ${BWDIR}/reuse/git/linux-tkg
+git fetch --all
+git reset --hard origin/master
+sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/zenbook-14-4700u.cfg|g" ${BWDIR}/reuse/git/linux-tkg/customization.cfg || true
 cd ${BWDIR}/reuse/git/linux-tkg
 makepkg -s 
-sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/zenbook-14-4700u.cfg|g" ${BWDIR}/reuse/git/linux-tkg/customization.cfg || true
-git pull origin || true
-sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/zenbook-14-4700u.cfg|g" ${BWDIR}/reuse/git/linux-tkg/customization.cfg || true
-cd ${BWDIR}/reuse/git/linux-tkg
-makepkg -s 
+
 cd ${BWDIR}
 pwd
 cp ${BWDIR}/reuse/git/linux-tkg/*.pkg.tar.zst ${BWDIR}/output/
