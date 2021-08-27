a44eab7921c2107a8eb1f159e92418fadc196425
Eile Kerning <tysayahi@gmail.com>
2021-08-27 08:46:07 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,11 +10,11 @@ echo "Kernel Complete"
 echo "Nvidia Start"
 cd ${BWDIR}
 touch ${BWDIR}/log/nvidia || true
-${BWDIR}/build-scripts/scripts/nvidia.sh >> ${BWDIR}/log/nvidia
+${BWDIR}/build-scripts/scripts/nvidia.sh 2>&1 | tee -a ${BWDIR}/log/nvidia
 echo "Nvidia Complete"
 cd ${BWDIR}
 cp ${BWDIR}/output/*.pkg.tar.zst ~/packages/
 echo "Packages Start"
 touch ${BWDIR}/log/packages || true
-${BWDIR}/build-scripts/scripts/packages.sh >> ${BWDIR}/log/packages
+${BWDIR}/build-scripts/scripts/packages.sh 2>&1 | tee -a ${BWDIR}/log/packages
 echo "Think this works."
