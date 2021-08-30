f70673dbb6a03dab79fcd33313ef4b8ecb4a7a9c
Eile Kerning <tysayahi@gmail.com>
2021-08-30 01:01:31 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,19 @@
 set -x
+date
 rm -rf ${BWDIR}/output/ || true
 mkdir ${BWDIR}/output/
 mkdir -p cd ${BWDIR}/reuse/git || true
 pwd
 mkdir -p ${BWDIR}/log/ || true
 
+date
 echo "Nvidia Start"
 cd ${BWDIR}
 touch ${BWDIR}/log/nvidia || true
 ${BWDIR}/build-scripts/scripts/nvidia.sh 2>&1 | tee -a ${BWDIR}/log/nvidia
 echo "Nvidia Complete"
 
+date
 cd ${BWDIR}
 cp ${BWDIR}/output/*.pkg.tar.zst ~/packages/
 echo "Packages Start"
@@ -18,8 +21,10 @@ touch ${BWDIR}/log/packages || true
 #Packages may fail to build unless keyserver-options auto-key-retrieve is in ~/.gnupg/gpg.conf
 ${BWDIR}/build-scripts/scripts/packages.sh 2>&1 | tee -a ${BWDIR}/log/packages
 
+date
 echo "Kernel Start"
 ${BWDIR}/build-scripts/scripts/kernel.sh 2>&1 | tee -a ${BWDIR}/log/kernel
 echo "Kernel Complete"
 
 echo "Think this works."
+date
