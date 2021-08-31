3d283b4e6d442b36cfefc211609da7bbbadcdcd1
Eile Kerning <tysayahi@gmail.com>
2021-08-30 22:20:39 -0400

    Update fetch_run.sh

--- scripts/fetch_run.sh
+++ scripts/fetch_run.sh
@@ -1,7 +1,7 @@
 set -x
 date
-touch ${BWDIR}/log/timestart || true
-date 2>&1 | tee -a ${BWDIR}/log/timestart
+touch ${BWDIR}/log/${date}/timestart || true
+date 2>&1 | tee -a ${BWDIR}/log/${date}/timestart
 pwd
 echo "Update Start"
     cd ..
@@ -12,5 +12,6 @@ cd ..
 BWDIR="$(pwd)"
 echo ${BWDIR}
 export BWDIR
-touch ${BWDIR}/log/build || true
-${BWDIR}/build-scripts/scripts/build.sh 2>&1 | tee -a ${BWDIR}/log/build
+mkdir -p ${BWDIR}/log/${date}/ || true
+touch ${BWDIR}/log/${date}/build || true
+${BWDIR}/build-scripts/scripts/build.sh 2>&1 | tee -a ${BWDIR}/log/${date}/build
