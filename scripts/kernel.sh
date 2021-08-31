95673ca1fab6115fd80cb6901345ecedf5f73016
Eile Kerning <tysayahi@gmail.com>
2021-08-30 22:22:04 -0400

    Update kernel.sh

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -1,7 +1,7 @@
 set -x
 cd ${BWDIR}
-touch ${BWDIR}/log/6700k || true
-touch ${BWDIR}/log/4700u || true
+touch ${BWDIR}/log/${date}/6700k || true
+touch ${BWDIR}/log/${date}/4700u || true
 mkdir -p ${BWDIR}/reuse/git || true
 mkdir -p ${BWDIR}/reuse/cfg/kernel || true
 cd ${BWDIR}/reuse/git
@@ -16,14 +16,14 @@ cd ${BWDIR}/reuse/git/linux-tkg
 git fetch --all
 git reset --hard origin/master
 sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/desktop-6700k.cfg|g" ${BWDIR}/reuse/git/linux-tkg/customization.cfg || true
-makepkg -s 
+makepkg -s 2>&1 | tee -a ${BWDIR}/log/${date}/6700k
 
 cd ${BWDIR}/reuse/git/linux-tkg
 git fetch --all
 git reset --hard origin/master
 sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/zenbook-14-4700u.cfg|g" ${BWDIR}/reuse/git/linux-tkg/customization.cfg || true
 cd ${BWDIR}/reuse/git/linux-tkg
-makepkg -s 
+makepkg -s 2>&1 | tee -a ${BWDIR}/log/${date}/4700u
 
 cd ${BWDIR}
 pwd
