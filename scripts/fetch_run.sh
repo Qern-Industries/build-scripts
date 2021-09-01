2f6a20a372aaded0a1d8b38e889406437b2e78dc
Eile Kerning <tysayahi@gmail.com>
2021-09-01 14:14:30 -0400

    Update fetch_run.sh

--- scripts/fetch_run.sh
+++ scripts/fetch_run.sh
@@ -1,12 +1,5 @@
 set -x
 date
-BWDIR="$(pwd)"
-tz=$(cat ${BWDIR}/build-scripts/timezone)
-repolocation=$(cat ${BWDIR}/build-scripts/repolocation)
-repo=$(cat ${BWDIR}/build-scripts/repo)
-date=$(TZ=${tz} date +"%d-%m-%Y-%S-%M-%H")
-touch ${BWDIR}/log/${date}/timestart || true
-date 2>&1 | tee -a ${BWDIR}/log/${date}/timestart
 pwd
 echo "Update Start"
     cd ..
@@ -14,6 +7,13 @@ echo "Update Start"
     git reset --hard origin/master
 echo "Update Complete"
 cd ..
+BWDIR="$(pwd)"
+tz=$(cat ${BWDIR}/build-scripts/timezone)
+repolocation=$(cat ${BWDIR}/build-scripts/repolocation)
+repo=$(cat ${BWDIR}/build-scripts/repo)
+date=$(TZ=${tz} date +"%d-%m-%Y-%S-%M-%H")
+touch ${BWDIR}/log/${date}/timestart || true
+date 2>&1 | tee -a ${BWDIR}/log/${date}/timestart
 echo ${BWDIR}
 export BWDIR
 mkdir -p ${BWDIR}/log/${date}/ || true
