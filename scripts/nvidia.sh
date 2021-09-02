4d2bf9d8d84ac6573cfa2f934b3bca9349f44777
Eile Kerning <tysayahi@gmail.com>
2021-09-02 14:05:53 -0400

    Update nvidia.sh

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -1,15 +1,15 @@
-mkdir -p ${BWDIR}/reuse/git || true
-mkdir -p ${BWDIR}/reuse/cfg/nvidia || true
-cd ${BWDIR}/reuse/git
+mkdir -p "${BWDIR}"/reuse/git || true
+mkdir -p "${BWDIR}"/reuse/cfg/nvidia || true
+cd "${BWDIR}"/reuse/git || exit
 git clone https://github.com/Frogging-Family/nvidia-all || true
-cd ${BWDIR}/reuse/git/nvidia-all
+cd "${BWDIR}"/reuse/git/nvidia-all || exit
 git fetch --all
 git reset --hard origin/master
-rm ${BWDIR}/reuse/cfg/nvidia/* || true
-cp ${BWDIR}/build-scripts/cfg/nvidia-5.13.cfg ${BWDIR}/reuse/cfg/nvidia/
-sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/nvidia-all.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/nvidia/nvidia-5.13.cfg|g" ${BWDIR}/reuse/git/nvidia-all/customization.cfg || true
-cd ${BWDIR}/reuse/git/nvidia-all
+rm "${BWDIR}"/reuse/cfg/nvidia/* || true
+cp "${BWDIR}"/build-scripts/cfg/nvidia-5.13.cfg "${BWDIR}"/reuse/cfg/nvidia/
+sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/nvidia-all.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/nvidia/nvidia-5.13.cfg|g" "${BWDIR}"/reuse/git/nvidia-all/customization.cfg || true
+cd "${BWDIR}"/reuse/git/nvidia-all || exit
 makepkg -s 
-cp ${BWDIR}/reuse/git/nvidia-all/*.pkg.tar.zst ${BWDIR}/output/
+cp "${BWDIR}"/reuse/git/nvidia-all/*.pkg.tar.zst "${BWDIR}"/output/
 
 
