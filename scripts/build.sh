d73be7ff286f6e501adc0821a116599cae18ea44
Eile Kerning <tysayahi@gmail.com>
2021-09-03 10:52:16 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,8 @@ mkdir "${BWDIR}"/output/
 mkdir -p "${BWDIR}"/reuse/git || true
 pwd
 mkdir -p "${BWDIR}"/log/"${date}" || true
+rm -rf "${repolocation}"/log/* || true
+mkdir -p "${repolocation}"/log/"${date}" || true
 
 date
 cd "${BWDIR}"
@@ -18,6 +20,7 @@ echo "Packages Start"
 touch "${BWDIR}"/log/"${date}"/packages || true
 #Packages may fail to build unless keyserver-options auto-key-retrieve is in ~/.gnupg/gpg.conf
 "${BWDIR}"/build-scripts/scripts/packages.sh 2>&1 | tee -a "${BWDIR}"/log/"${date}"/packages
+cp -r "${BWDIR}/log/"${date}"/* "${repolocation}"/log/
 
 date
 echo "Nvidia Start"
@@ -25,15 +28,14 @@ cd "${BWDIR}"
 touch "${BWDIR}"/log/"${date}"/nvidia || true
 "${BWDIR}"/build-scripts/scripts/nvidia.sh 2>&1 | tee -a "${BWDIR}"/log/"${date}"/nvidia
 echo "Nvidia Complete"
+cp -r "${BWDIR}/log/"${date}"/* "${repolocation}"/log/
 
 date
 echo "Kernel Start"
 "${BWDIR}"/build-scripts/scripts/kernel.sh 2>&1 | tee -a "${BWDIR}"/log/"${date}"/kernel
 echo "Kernel Complete"
 repo-add "${repolocation}"/"${repo}".db.tar.gz "${repolocation}"/*.pkg.tar.zst
+cp -r "${BWDIR}/log/"${date}"/* "${repolocation}"/log/
 
 touch "${BWDIR}"/log/"${date}"/timeend || true
 date 2>&1 | tee -a "${BWDIR}"/log/"${date}"/timeend
-rm -rf "${repolocation}"/log/* || true
-mkdir -p "${repolocation}"/log/"${date}" || true
-cp -r "${BWDIR}/log/"${date}"/* "${repolocation}"/log/
