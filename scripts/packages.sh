57d7bed7c0e8b7ce22311653764c21fe4d8740a3
Eile Kerning <tysayahi@gmail.com>
2021-09-03 10:49:24 -0400

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -3,5 +3,5 @@ set -x
 cd "${BWDIR}" || exit
 list_unsorted="$(cat "${BWDIR}"/build-scripts/list)"
 list="${list_unsorted//$'\n'/ }"
-echo "$list"
-aur sync -uT --no-view --ignore=python-haishoku "$(list)"
+echo "${list}"
+aur sync -uT --no-view --ignore=python-haishoku "${list}"
