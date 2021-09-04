7a167eae708354af1be1b5dcc34a52b99770d757
Eile Kerning <tysayahi@gmail.com>
2021-09-04 19:15:16 -0400

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env -S bash
 set -x
 cd "${BWDIR}" || exit
-list_unsorted="$(cat "${BWDIR}"/build-scripts/list)"
-
-echo "$list_unsorted" 
-
 while read arg; do
-    aur sync -cuT --no-view "$arg"
+    aur sync -cuT --no-view "$arg" || true
 done < ""${BWDIR}"/build-scripts/list"
-
-exit
-
-\n
-\n
-
-#list_sorted="${list_unsorted//$'\n'/ }"
-#list="$(echo "$list_sorted")"
-#echo "${list[@]}"
-#eval aur sync -cuT --no-view --ignore=python-haishoku "${list}"
