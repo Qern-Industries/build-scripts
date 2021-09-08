d8502624d06a8de1993a92523a883f8c13478215
Eile Kerning <tysayahi@gmail.com>
2021-09-08 11:00:01 -0400

    Create cron.sh

---
+++ scripts/cron.sh
@@ -0,0 +1,4 @@
+#0 5 * * * cd /home/eile/build/build-scripts/scripts/ && ./cron.sh
+crondate=$(TZ="${tz}" date +"%d-%m-%Y-%H-%M-%S")
+mkdir -p ~/cronlog/ || true
+./fetch_run.sh 2>&1 | tee -a ~/cronlog/"${crondate}
