4ea9245c8ff4b022850dff907f35c1a66e81df95
Eile Kerning <tysayahi@gmail.com>
2021-09-14 10:30:49 -0400

    Update fetch_run.sh

--- scripts/fetch_run.sh
+++ scripts/fetch_run.sh
@@ -9,14 +9,11 @@ echo "Update Complete"
 cd ..
 BWDIR=$(pwd)
 tz=$(cat "${BWDIR}"/build-scripts/timezone)
-repo=$(cat "${BWDIR}"/build-scripts/repo)
 date=$(TZ="${tz}" date +"%d-%m-%Y-%H-%M-%S")
 echo "${BWDIR}"
 mkdir -p ~/packages/log/"${date}"/ || true
 touch ~/packages/log/"${date}"/build || true
 export BWDIR
 export date
-export repo
-export repolocation
 export tz
 "${BWDIR}"/build-scripts/scripts/build.sh 2>&1 | tee -a ~/packages/log/"${date}"/build
