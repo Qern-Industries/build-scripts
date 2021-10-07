4ecb9ac490d536d289b47216e1fe2ca557075bf1
Eile Kerning <tysayahi@gmail.com>
2021-10-07 10:08:31 -0400

    Update cron.sh

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,4 +1,10 @@
+
+#!/usr/bin/env -S bash -euET -o pipefail -O inherit_errexit
 #0 5 * * * cd /home/eile/build/build-scripts/scripts/ && ./cron.sh
-crondate=$(date +"%d-%m-%Y-%H-%M-%S")
+#crondate=$(date +"%d-%m-%Y-%H-%M-%S")
+cronyear=$(date +"%Y")
+cronmonth=$(date +"%m")
+cronday=$(date +"%d")
+crontime=$(date +"%H-%M-%S")
 mkdir -p ~/packages/cronlog/ || true
-./fetch_run.sh 2>&1 | tee -a ~/packages/cronlog/"${crondate}"
+./fetch_run.sh 2>&1 | tee -a ~/packages/cronlog/"${cronyear}/${cronmonth}/${cronday}/${crontime}"
