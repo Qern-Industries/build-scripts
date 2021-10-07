88dcbf11e0198631383e0aa0045a50013258533e
Eile Kerning <tysayahi@gmail.com>
2021-10-07 09:55:08 -0400

    Update cron.sh

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,4 +1,4 @@
 #0 5 * * * cd /home/eile/build/build-scripts/scripts/ && ./cron.sh
-crondate=$(TZ="${tz}" date +"%d-%m-%Y-%H-%M-%S")
+crondate=$(date +"%d-%m-%Y-%H-%M-%S")
 mkdir -p ~/packages/cronlog/ || true
 ./fetch_run.sh 2>&1 | tee -a ~/packages/cronlog/"${crondate}"
