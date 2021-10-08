785c0c3359a957e009b406fc1b326d53201c94bc
Eile Kerning <tysayahi@gmail.com>
2021-10-08 14:10:26 -0400

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -2,5 +2,5 @@
 set -x
 cd "${BWDIR}" || exit
 while read -r arg; do
-    date; time aur sync -cuT --no-view "$arg" || true
+    date; time aur sync -cuT --no-view --margs --skipinteg "$arg" || true
 done < "${BWDIR}/build-scripts/list"
