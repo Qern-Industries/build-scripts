176ea8fb5d793ab954373143adace11601d9d5b8
Eile Kerning <tysayahi@gmail.com>
2021-10-15 19:49:45 -0400

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -2,5 +2,5 @@
 set -x
 cd "${BWDIR}" || exit
 while read -r arg; do
-    date; time base_packages=('base-devel' 'multilib-devel' 'gcc-git') aur sync -cuT --no-view --margs --noconfirm "$arg" || true
+    date; time base_packages=('base-devel' 'multilib-devel' 'gcc-git') aur sync -uT --no-view --margs --noconfirm "$arg" || true
 done < "${BWDIR}/build-scripts/list"
