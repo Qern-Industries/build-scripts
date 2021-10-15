5fc11bfc24693e0f0960cfbdca912213bbf4a54e
Orangian <tysayahi@gmail.com>
2021-10-15 11:08:20 -0400

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -2,5 +2,5 @@
 set -x
 cd "${BWDIR}" || exit
 while read -r arg; do
-    date; time aur sync -cuT --no-view --margs --noconfirm --margs --skipinteg "$arg" || true
+    date; time base_packages=('base-devel' 'multilib-devel' 'gcc-git') aur sync -cuT --no-view --margs --noconfirm "$arg" || true
 done < "${BWDIR}/build-scripts/list"
