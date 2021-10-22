1857bfa9ba4fde8b35d8e51ae00bd99a0b4a15fa
Eile Kerning <tysayahi@gmail.com>
2021-10-22 11:57:20 -0400

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env -S bash
 set -x
-mkdir -p "${BWDIR}/build"
-cd "${BWDIR}/build" || exit
-while read -r arg; do
-    mkdir -p ~/packages/git/ || true
-    cd ~/packages/git/ || true
-    date; time aur fetch -r --sync=reset $arg || true
-done < "${BWDIR}/build-scripts/list"
-while read -r arg; do
-    cd ~/packages/git/"${arg}"
-    date; time aur build --no-sync --margs -n,-s,--clean $arg || true
-done < "${BWDIR}/build-scripts/list"
-cd "${BWDIR}"
+date; time aur sync -uT --no-view --margs -n,-s,--clean "$(cat ${BWDIR}/build-scripts/list)" || true
