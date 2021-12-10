c9274be7bd790d631d1369b3e7a4b4d8d2478608
Eile Kerning <tysayahi@gmail.com>
2021-12-10 12:53:57 -0500

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env -S bash
 set -x
-date; time cat /home/eile/passphrase | aur sync -uT --no-view --margs --asdeps,--needed,--noconfirm,--noprogressbar,-C,-c,-s,-r,--sign "$(cat ${BWDIR}/build-scripts/list)" || true
+date; time aur sync -uT --no-view --margs --asdeps,--needed,--noconfirm,--noprogressbar,-C,-c,-s,-r,--sign "$(cat ${BWDIR}/build-scripts/list)" || true
