993162a23e34be68d86025674f89cb2b6f4062e3
Eile Kerning <tysayahi@gmail.com>
2021-12-14 10:39:07 -0500

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env -S bash
 set -x
 bash ~/cache-gpg.sh
-date; time aur sync -uTf --no-view --margs --asdeps,--needed,--noconfirm,--noprogressbar,-C,-c,-s,-r --sign "$(cat ${BWDIR}/build-scripts/list)" || true
+date; time aur sync -uT --no-view --margs --asdeps,--needed,--noconfirm,--noprogressbar,-C,-c,-s,-r "$(cat ${BWDIR}/build-scripts/list)" || true
