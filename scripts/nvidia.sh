b08a8bf1f66ee3c304ef560a48198e05204bb869
Eile Kerning <tysayahi@gmail.com>
2021-12-14 10:54:30 -0500

    Update nvidia.sh

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -10,7 +10,6 @@ rm "${BWDIR}"/reuse/cfg/nvidia/* || true
 cp "${BWDIR}"/build-scripts/cfg/nvidia.cfg "${BWDIR}"/reuse/cfg/nvidia/
 sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/nvidia-all.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/nvidia/nvidia.cfg|g" "${BWDIR}"/reuse/git/nvidia-all/customization.cfg || true
 cd "${BWDIR}"/reuse/git/nvidia-all || exit
-bash ~/cache-gpg.sh
-makepkg -sCc || true
+makepkg -sfCc || true
 cp "${BWDIR}"/reuse/git/nvidia-all/*.pkg.tar.zst ~/packages/
 
