6c7029d4ca7d9963c052beb6dc8f31a600cbee1f
Eile Kerning <tysayahi@gmail.com>
2022-01-28 14:44:18 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,8 @@ _qi_build_month=$(TZ=US/New_York date +"%m")
 _qi_build_day=$(TZ=US/New_York date +"%d")
 _qi_build_time=$(TZ=US/New_York date +"%H-%M")
 
+export _qi_build_{day,month,year,time}
+
 echo "Update Start"
     git fetch --all || exit
     git reset --hard origin/master || exit
@@ -16,4 +18,4 @@ echo "Update Complete"
 mkdir -p ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/ || exit
 touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/log || true
 
-./main.sh 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/main.sh || exit
+./main.sh 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/main || exit
