b1b722d8c8596f51539b45db99c0facf5e92ec57
Orangian <tysayahi@gmail.com>
2022-01-28 10:31:29 -0500

    Git add -A...

---
+++ scripts/proton.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+set -x
+cd "${BWDIR}" || exit
+rm -rf "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/*.pkg.tar.zst
+touch ~/packages/cronlog/"${year}/${month}/${day}/${time}"/6700k || true
+touch ~/packages/cronlog/"${year}/${month}/${day}/${time}"/4700u || true
+mkdir -p "${BWDIR}"/reuse/git || true
+mkdir -p "${BWDIR}"/reuse/cfg/kernel || true
+cd "${BWDIR}"/reuse/git || exit
+git clone https://github.com/Frogging-Family/linux-tkg || true
+cd "${BWDIR}"/reuse/git/linux-tkg || exit
+git pull origin || true
+rm "${BWDIR}"/reuse/cfg/kernel/* || true
+cp "${BWDIR}"/build-scripts/cfg/linux-tkg/laptop-4700u.cfg "${BWDIR}"/reuse/cfg/kernel/
+cp "${BWDIR}"/build-scripts/cfg/linux-tkg/desktop-6700k.cfg "${BWDIR}"/reuse/cfg/kernel/
+
+sudo pacman -S --noconfirm --needed schedtool || true
+cd "${BWDIR}"/reuse/git/linux-tkg || exit
+git fetch --all
+git reset --hard origin/master
+sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/desktop-6700k.cfg|g" "${BWDIR}"/reuse/git/linux-tkg/customization.cfg || true
+makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${year}/${month}/${day}/${time}"/6700k || exit
+
+cd "${BWDIR}"/reuse/git/linux-tkg || exit
+git fetch --all
+git reset --hard origin/master
+sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/kernel/laptop-4700u.cfg|g" "${BWDIR}"/reuse/git/linux-tkg/customization.cfg || true
+makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${year}/${month}/${day}/${time}"/4700u || exit
+
+cd "${BWDIR}" || exit
+pwd
+cp "${BWDIR}"/reuse/git/linux-tkg/*.pkg.tar.zst ~/packages/
+
