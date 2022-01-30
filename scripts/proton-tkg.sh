d9428b726664e4a4223eeaa402e96cffd7246257
Eile Kerning <tysayahi@gmail.com>
2022-01-30 18:29:44 -0500

    Update proton-tkg.sh

--- scripts/proton-tkg.sh
+++ scripts/proton-tkg.sh
@@ -22,9 +22,9 @@ git reset --hard origin/master
 git fetch --all
 git reset --hard origin/master
 rm -rf "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg.cfg
-cp "${BWDIR}"/reuse/cfg/proton-tkg/proton.cfg "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg.cfg
+cp "${BWDIR}"/build-scripts/cfg/proton-tkg/proton.cfg "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg.cfg
 rm -rf "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg-profiles/advanced-customization.cfg 
-cp "${BWDIR}"/reuse/cfg/proton-tkg/proton-advanced-6700k.cfg "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg-profiles/advanced-customization.cfg 
+cp "${BWDIR}"/build-scripts/cfg/proton-tkg/proton-advanced-6700k.cfg "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg-profiles/advanced-customization.cfg 
 makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/proton-tkg/desktop-6700k || exit
 
 git fetch --all
@@ -32,7 +32,7 @@ git reset --hard origin/master
 rm -rf "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg.cfg
 cp "${BWDIR}"/reuse/cfg/proton-tkg/proton.cfg "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg.cfg
 rm -rf "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg-profiles/advanced-customization.cfg 
-cp "${BWDIR}"/reuse/cfg/proton-tkg/proton-advanced-4700u.cfg "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg-profiles/advanced-customization.cfg 
+cp "${BWDIR}"/build-scripts/cfg/proton-tkg/proton-advanced-4700u.cfg "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg-profiles/advanced-customization.cfg 
 makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/proton-tkg/laptop-4700u || exit
 
 cd "${BWDIR}" || exit
