9ac81c8ef4c2c06b4406f6178bcba0650a351410
Eile Kerning <tysayahi@gmail.com>
2022-01-30 18:26:16 -0500

    Update wine-tkg.sh

--- scripts/wine-tkg.sh
+++ scripts/wine-tkg.sh
@@ -20,20 +20,20 @@ git fetch --all
 git reset --hard origin/master
 
 rm -rf "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/customization.cfg
-cp "${BWDIR}"/reuse/cfg/wine-tkg-git/wine.cfg "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/customization.cfg
+cp "${BWDIR}"/build-scripts/cfg/wine-tkg/wine.cfg "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/customization.cfg
 
 git fetch --all
 git reset --hard origin/master
 cd "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/ || exit
 rm -rf "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/advanced-customization.cfg 
-cp "${BWDIR}"/reuse/cfg/wine-tkg-git/wine-advanced-6700k.cfg "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/advanced-customization.cfg 
+cp "${BWDIR}"/build-scripts/cfg/wine-tkg/wine-advanced-6700k.cfg "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/advanced-customization.cfg 
 makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/wine-tkg-git/desktop-6700k || exit
 
 git fetch --all
 git reset --hard origin/master
 cd "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/ || exit
 rm -rf "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/advanced-customization.cfg 
-cp "${BWDIR}"/reuse/cfg/wine-tkg-git/wine-advanced-4700u.cfg "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/advanced-customization.cfg 
+cp "${BWDIR}"/build-scripts/cfg/wine-tkg/wine-advanced-4700u.cfg "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/advanced-customization.cfg 
 makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/wine-tkg-git/laptop-4700u || exit
 
 cd "${BWDIR}" || exit
