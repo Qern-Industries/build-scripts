ed94aeb1fd92fa4a2295dc4a26507b9cbd879867
Eile Kerning <tysayahi@gmail.com>
2022-01-31 10:41:11 -0500

    Update linux-tkg.sh

--- scripts/linux-tkg.sh
+++ scripts/linux-tkg.sh
@@ -2,8 +2,9 @@
 set -x
 cd "${BWDIR}" || exit
 
-touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/desktop-6700k || true
-touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/laptop-4700u || true
+touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/desktop-skylake || true
+touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/laptop-zen2 || true
+touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/desktop-zen2 || true
 
 mkdir -p "${BWDIR}"/reuse/git || true
 mkdir -p "${BWDIR}"/reuse/cfg/linux-tkg || true
@@ -15,27 +16,29 @@ cd "${BWDIR}"/reuse/git/linux-tkg || exit
     git reset --hard origin/master || exit
 
 rm "${BWDIR}"/reuse/cfg/linux-tkg/* || true
-cp "${BWDIR}"/build-scripts/cfg/linux-tkg/laptop-4700u.cfg "${BWDIR}"/reuse/cfg/linux-tkg/
-cp "${BWDIR}"/build-scripts/cfg/linux-tkg/desktop-6700k.cfg "${BWDIR}"/reuse/cfg/linux-tkg/
+cp "${BWDIR}"/build-scripts/cfg/linux-tkg/laptop-zen2.cfg "${BWDIR}"/reuse/cfg/linux-tkg/
+cp "${BWDIR}"/build-scripts/cfg/linux-tkg/desktop-skylake.cfg "${BWDIR}"/reuse/cfg/linux-tkg/
+cp "${BWDIR}"/build-scripts/cfg/linux-tkg/desktop-zen2.cfg "${BWDIR}"/reuse/cfg/linux-tkg/
+
 
 sudo pacman -S --noconfirm --needed schedtool || true
 cd "${BWDIR}"/reuse/git/linux-tkg || exit
 git fetch --all
 git reset --hard origin/master
-sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/linux-tkg/desktop-6700k.cfg|g" "${BWDIR}"/reuse/git/linux-tkg/customization.cfg || true
-makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/desktop-6700k || exit
+sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/linux-tkg/desktop-skylake.cfg|g" "${BWDIR}"/reuse/git/linux-tkg/customization.cfg || true
+makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/desktop-skylake || exit
 
 cd "${BWDIR}"/reuse/git/linux-tkg || exit
 git fetch --all
 git reset --hard origin/master
-sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/linux-tkg/laptop-4700u.cfg|g" "${BWDIR}"/reuse/git/linux-tkg/customization.cfg || true
-makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/laptop-4700u || exit
+sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/linux-tkg/laptop-zen2.cfg|g" "${BWDIR}"/reuse/git/linux-tkg/customization.cfg || true
+makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/laptop-zen2 || exit
 
 cd "${BWDIR}"/reuse/git/linux-tkg || exit
 git fetch --all
 git reset --hard origin/master
-sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/linux-tkg/desktop-3600.cfg|g" "${BWDIR}"/reuse/git/linux-tkg/customization.cfg || true
-makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/desktop-3600 || exit
+sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/linux-tkg/desktop-zen2.cfg|g" "${BWDIR}"/reuse/git/linux-tkg/customization.cfg || true
+makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/desktop-zen2 || exit
 
 
 cd "${BWDIR}" || exit
