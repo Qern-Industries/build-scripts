8192a8a98f7dbee44ca1b6b183612287eb06abd0
Eile Kerning <tysayahi@gmail.com>
2022-02-03 11:20:47 -0500

    Update wine-tkg.sh

--- scripts/wine-tkg.sh
+++ scripts/wine-tkg.sh
@@ -2,7 +2,6 @@
 set -x
 cd "${BWDIR}" || exit
 
-mkdir -p ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/wine-tkg-git/
 touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/wine-tkg-git/skylake || true
 touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/wine-tkg-git/zen2 || true
 
