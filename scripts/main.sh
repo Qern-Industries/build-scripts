440d626c720ee6396cc8f09f79edbb60230aa3e2
Eile Kerning <tysayahi@gmail.com>
2022-02-07 11:33:33 -0500

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -16,30 +16,30 @@ echo "Packages Complete"
 echo "Nvidia Start"
 cd "${BWDIR}"
 mkdir -p ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/nvidia-tkg/
-time "${BWDIR}"/build-scripts/scripts/nvidia-tkg.sh 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/nvidia-tkg/nvidia-tkg
+#time "${BWDIR}"/build-scripts/scripts/nvidia-tkg.sh 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/nvidia-tkg/nvidia-tkg
 echo "Nvidia Complete"
-eval repo-add -n ~/packages/qern-packs.db.tar.gz ~/packages/*.pkg.tar.zst
+#eval repo-add -n ~/packages/qern-packs.db.tar.gz ~/packages/*.pkg.tar.zst
 
 echo "Kernel Start"
 cd "${BWDIR}"
 mkdir -p ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/
-time "${BWDIR}"/build-scripts/scripts/linux-tkg.sh 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/linux-tkg
+#time "${BWDIR}"/build-scripts/scripts/linux-tkg.sh 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/linux-tkg
 echo "Kernel Complete"
-eval repo-add -n ~/packages/qern-packs.db.tar.gz ~/packages/*.pkg.tar.zst
+#eval repo-add -n ~/packages/qern-packs.db.tar.gz ~/packages/*.pkg.tar.zst
 
 echo "Wine Start"
 cd "${BWDIR}"
 mkdir -p ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/wine-tkg-git/
-time "${BWDIR}"/build-scripts/scripts/wine-tkg.sh 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/wine-tkg-git/wine-tkg-git
+#time "${BWDIR}"/build-scripts/scripts/wine-tkg.sh 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/wine-tkg-git/wine-tkg-git
 echo "Wine Complete"
-eval repo-add -n ~/packages/qern-packs.db.tar.gz ~/packages/*.pkg.tar.zst
+#eval repo-add -n ~/packages/qern-packs.db.tar.gz ~/packages/*.pkg.tar.zst
 
 echo "Proton Start"
 cd "${BWDIR}"
-mkdir -p ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/proton-tkg/
+#mkdir -p ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/proton-tkg/
 time "${BWDIR}"/build-scripts/scripts/proton-tkg.sh 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/proton-tkg/proton-tkg
 echo "Proton Complete"
-eval repo-add -n ~/packages/qern-packs.db.tar.gz ~/packages/*.pkg.tar.zst
+#eval repo-add -n ~/packages/qern-packs.db.tar.gz ~/packages/*.pkg.tar.zst
 
 ccache -sv
 
