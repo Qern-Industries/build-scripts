712a6279e4c3f9043670751bb0a6a53bb443ea31
Orangian <tysayahi@gmail.com>
2022-02-09 13:25:46 -0500

    Temp disable removal

--- main.sh
+++ main.sh
@@ -30,4 +30,4 @@ touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/
 date 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/timeend
 
 lrztar -L 9 -z ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/
-rm -rf ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/
+#rm -rf ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/
