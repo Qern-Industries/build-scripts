ca2b23384980be4c3efc15041841dc3c1616b520
Eile Kerning <tysayahi@gmail.com>
2022-02-10 11:14:37 -0500

    Update qern-packages.sh

--- scripts/qern-packages.sh
+++ scripts/qern-packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env -S bash
 #Packages may fail to build unless keyserver-options auto-key-retrieve is in ~/.gnupg/gpg.conf
 
-#date; time aur sync -uT --no-view --margs --asdeps,--needed,--noconfirm,--noprogressbar,-C,-c,-s,-r "$(cat ${BWDIR}/build-scripts/lists/*/*)" || exit
+date; time aur sync -uT --no-view --margs --asdeps,--needed,--noconfirm,--noprogressbar,-C,-c,-s,-r "$(cat ${BWDIR}/build-scripts/lists/*/*)" || exit
