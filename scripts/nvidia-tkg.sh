e75d9e9a8b5858fd9ccd214f2c7c2e5492ff471f
Eile Kerning <tysayahi@gmail.com>
2022-02-14 10:44:37 -0500

    Create nvidia-tkg.sh

--- scripts/nvidia-tkg.sh
+++ scripts/nvidia-tkg.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/bash
-
+    if [[ $(cat "${BWDIR}/build-scripts/switches/debug" > /dev/null; echo $?) = 1 ]]; then
+       :
+    elif [[ $(cat "${BWDIR}/build-scripts/switches/debug" > /dev/null; echo $?) = 0 ]]; then
+         if [[ $(cat "${BWDIR}/build-scripts/switches/debug") = 0 ]]; then
+              :
+         elif [[ $(cat "${BWDIR}/build-scripts/switches/debug") = 1 ]]; then 
+              echo "Debug enabled, setting -x."
+              set -x
+         else
+              echo "Unexpected condition, exiting."
+              exit
+         fi
+    else
+       echo "Unexpected condition, exiting." 
+    fi     
 mkdir -p "${BWDIR}"/reuse/git || true
 mkdir -p "${BWDIR}"/reuse/cfg/nvidia || true
 
