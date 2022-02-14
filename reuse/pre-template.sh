4b33f8540f0b75d3cbc10011d92d596fb971b4c5
Eile Kerning <tysayahi@gmail.com>
2022-02-14 10:43:49 -0500

    Update pre-template.sh

--- reuse/pre-template.sh
+++ reuse/pre-template.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+    if [[ $(cat "${BWDIR}/build-scripts/switches/debug" > /dev/null; echo $?) = 1 ]]; then
+       :
+    elif [[ $(cat "${BWDIR}/build-scripts/switches/debug" > /dev/null; echo $?) = 0 ]]; then
+         if [[ $(cat "${BWDIR}/build-scripts/switches/debug") = 0 ]]; then
+              :
+         elif [[ $(cat "${BWDIR}/build-scripts/switches/debug") = 1 ]]; then 
+              echo "Debug enabled, setting -x."
+              set -x
+         else
+              echo "Unexpected condition, exiting."
+              exit
+         fi
+    else
+       echo "Unexpected condition, exiting." 
+    fi     
+
 echo "${_qi_act_fancy} Start"
 cd "${BWDIR}"
 mkdir -p ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/${_qi_act_script}/"
