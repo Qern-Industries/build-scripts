38bd87e6d4634f8960201272cada0c169e3371c0
Eile Kerning <tysayahi@gmail.com>
2022-02-14 10:44:30 -0500

    Update linux-tkg.sh

--- scripts/linux-tkg.sh
+++ scripts/linux-tkg.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/bash
-
+    if [[ $(cat "${BWDIR}/build-scripts/switches/debug" > /dev/null; echo $?) = 1 ]]; then
+       :
+    elif [[ $(cat "${BWDIR}/build-scripts/switches/debug" > /dev/null; echo $?) = 0 ]]; then
+         if [[ $(cat "${BWDIR}/build-scripts/switches/debug") = 0 ]]; then
+              :
+         elif [[ $(cat "${BWDIR}/build-scripts/switches/debug") = 1 ]]; then 
+              echo "Debug enabled, setting -x."
+              set -x
+         else
+              echo "Unexpected condition, exiting."
+              exit
+         fi
+    else
+       echo "Unexpected condition, exiting." 
+    fi     
 cd "${BWDIR}" || exit
 
 touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/desktop-skylake || true
