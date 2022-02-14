d83d54af95456cc0189a43a171e0571a34e6c746
Eile Kerning <tysayahi@gmail.com>
2022-02-14 10:43:34 -0500

    Update export-essential.sh

--- reuse/export-essential.sh
+++ reuse/export-essential.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/bash
 
+    if [[ $(cat "${BWDIR}/build-scripts/switches/debug" > /dev/null; echo $?) = 1 ]]; then
+       :
+    elif [[ $(cat "${BWDIR}/build-scripts/switches/debug" > /dev/null; echo $?) = 0 ]]; then
+         if [[ $(cat "${BWDIR}/build-scripts/switches/debug") = 0 ]]; then
+              :
+         elif [[ $(cat "${BWDIR}/build-scripts/switches/debug") = 1 ]]; then 
+              echo "Debug enabled, setting -x."
+              set -x
+         else
+              echo "Unexpected condition, exiting."
+              exit
+         fi
+    else
+       echo "Unexpected condition, exiting." 
+    fi     
+
 export BWDIR _qi_build_{day,month,year,time}
