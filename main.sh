beef406c29111b2ec151d9c622322a9fc223679a
Orangian <tysayahi@gmail.com>
2022-03-17 14:05:56 -0400

    Fix proton-tkg logging over wine-tkg logs

--- main.sh
+++ main.sh
@@ -143,7 +143,7 @@ proton-tkg () {
      cp "${BWDIR}"/build-scripts/cfg/proton-tkg/proton.cfg "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg.cfg
      cd "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/ || exit
      eval sed -i 's/pkgname=proton-tkg-git/pkgname=proton-tkg-${_qi_target_nice}-qern/g' PKGBUILD
-     makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/wine-tkg/${_qi_target_nice}" || exit
+     makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/proton-tkg/${_qi_target_nice}" || exit
      mv "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/*.pkg.tar.zst ~/packages/ || true
      done < "${BWDIR}/build-scripts/wine-targets"
 }
