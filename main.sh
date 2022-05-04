490b163a1db96fb31477f3c68f28dcf567d6f231
Eile Kerning <tysayahi@gmail.com>
2022-05-04 08:44:20 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -105,6 +105,7 @@ linux-tkg-pkgbuild () {
 }
 
 qern-packages () {
+     sudo pacman -Syu --noconfirm
      date; time aur sync -uT --no-view --margs --asdeps,--needed,--noconfirm,--noprogressbar,-C,-c,-s,-r "$(cat ${BWDIR}/build-scripts/lists/*/*)" || exit
 }
 
