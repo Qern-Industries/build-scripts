29ccaee35d8b09c83ecb53824e4890ac4775e3f5
Ty Ethan Sayahi <tysayahi@gmail.com>
2022-05-28 06:07:10 -0400

    Replace old main.sh

--- main.sh
+++ main.sh
@@ -1,176 +1,120 @@
 #!/usr/bin/bash
 main () {
-     # Set Base working dir.
-     cd ..
-     BWDIR=$(pwd)
+    # Set base directory
+    cd ..
+    BWDIR=$(pwd)
 
-     # Check if debug is enabled; if so, set -x
+    mkdir -p ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/timeend
 
-     if cat "${BWDIR}/build-scripts/debug"; then
-          if [[ $(cat "${BWDIR}/build-scripts/debug") = 0 ]]; then
-               :
-          else
-               echo "Debug enabled, setting -x."
-               set -x
-          fi
-     else
-          :
-     fi     
+    set -e
 
-     export BWDIR
+    # Check if debug is enabled, and if so, set -x
+    if [[  -f "${BWDIR}/build-scripts/debug" ]]; then
+        echo "Debug enabled, setting -x."
+        set -x
+    fi
 
+    export BWDIR
 
-     while IFS= read -r _qi_build_arg; do
-     export ${_qi_build_arg}
-     if [[ "${_qi_act_switch}" = 1 ]]; then
-               echo "Switch for $_qi_act_fancy enabled, running."
-               pre-script
-     else
-               echo "Switch for $_qi_act_fancy disabled, not running."
-     fi
-     done < "${BWDIR}/build-scripts/script-list"
+    while IFS= read -r _qi_build_arg; do
+    export ${_qi_build_arg}
+    cd "${BWDIR}"
+    if [[ "${_qi_act_switch}" = 1 ]]; then
+            echo "Switch for $_qi_act_fancy enabled, running."
+            pre-script
+    else
+            echo "Switch for $_qi_act_fancy disabled, not running."
+    fi
+    done < "${BWDIR}/build-scripts/script-list"
 
-     ccache -sv 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/ccache
-
-     touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/timeend || true
-     date 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/timeend
-
-
-
-     # Check if debug is enabled; if so, don't compress logs.
-
-     if cat "${BWDIR}/build-scripts/debug"; then
-                    if [[ $(cat "${BWDIR}/build-scripts/debug") = 0 ]]; then
-                         cd ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}"/ || exit
-                         lrztar -L 9 -z ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/
-                         cd "${BWDIR}" || exit
-                         rm -rf ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/
-                    elif [[ $(cat "${BWDIR}/build-scripts/debug") = 1 ]]; then 
-                         echo "Debug enabled, not compressing logs."
-                    else
-                         echo "Unexpected condition, exiting."
-                         exit
-                    fi
-     else
-          :
-     fi                
-
+    ccache -sv 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/ccache
+    date 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/timeend
+    if [[ ! -f "${BWDIR}/build-scripts/debug" ]]; then
+        cd ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}"/ || exit
+        lrztar -L 9 -z ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/
+        cd "${BWDIR}" || exit
+        rm -rf ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/
+    fi
+    exit
 }
 
 pre-script () {
-     echo "${_qi_act_fancy} Start"
-     cd "${BWDIR}"
-     mkdir -p ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/${_qi_act_script}/"
-     time ${_qi_act_script} 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/${_qi_act_script}/${_qi_act_script}" || exit
-     echo "${_qi_act_fancy} Complete"
-
-     if [[ "${_qi_act_script}" = qern-packages ]]; then
-     echo "$_qern_act_fancy} detected. Not repo-adding."
-     else
-     eval repo-add -n ~/packages/qern-packs.db.tar.gz ~/packages/*.pkg.tar.zst
-     fi
+    rm -rf "${BWDIR}"/work/ || true
+    time ${_qi_act_script} 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/${_qi_act_script}/${_qi_act_script}" || exit
+    echo "${_qi_act_fancy} Complete"
+    if [[ "${_qi_act_script}" = qern-packages ]]; then
+        echo "$_qern_act_fancy} detected. Not repo-adding."
+    else
+        rm -rf ~/packages/qern-packs.*
+        eval repo-add -n ~/packages/qern-packs.db.tar.gz ~/packages/*.pkg.tar.zst
+    fi
 }
-
-
 
 linux-tkg () {
-     touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/{desktop-{skylake,zen2},laptop-zen2} || true
-     mkdir -p "${BWDIR}"/reuse/{git,cfg/linux-tkg} || true
-     cd "${BWDIR}"/reuse/git || exit
-     git clone https://github.com/Frogging-Family/linux-tkg || true
-     cd "${BWDIR}"/reuse/git/linux-tkg || exit
-     git fetch --all || exit
-     git reset --hard origin/master || exit
-     rm "${BWDIR}"/reuse/cfg/linux-tkg/* || true
-     cp "${BWDIR}"/build-scripts/cfg/linux-tkg/*.cfg "${BWDIR}"/reuse/cfg/linux-tkg/
-     sudo pacman -S --noconfirm --needed schedtool || true
-     
-     _qi_lintkg_target=desktop-skylake
-     linux-tkg-pkgbuild
-     _qi_lintkg_target=desktop-zen2
-     linux-tkg-pkgbuild
-     _qi_lintkg_target=laptop-zen2
-     linux-tkg-pkgbuild
-     
-     cd "${BWDIR}" || exit
-     mv "${BWDIR}"/reuse/git/linux-tkg/*.pkg.tar.zst ~/packages/
-}
-
-linux-tkg-pkgbuild () {
-     cd "${BWDIR}"/reuse/git/linux-tkg || exit
-     git fetch --all
-     git reset --hard origin/master
-     sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/linux-tkg/${_qi_lintkg_target}.cfg|g" "${BWDIR}"/reuse/git/linux-tkg/customization.cfg || true
-     makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/${_qi_lintkg_target} || exit
+    while IFS= read -r _qi_linux_arg; do
+    export ${_qi_linux_arg}
+    rm -rf "${BWDIR}"/work/ || true
+    mkdir -p "${BWDIR}/work/"
+    cd "${BWDIR}/work/"
+    git clone https://github.com/Frogging-Family/linux-tkg || true
+    cd "${BWDIR}/work/linux-tkg"
+    sudo pacman -Syu --noconfirm --needed schedtool || true
+    sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/linux-tkg.cfg|_EXT_CONFIG_PATH=${BWDIR}/build-scripts/cfg/linux-tkg/${_qi_lintkg_target}.cfg|g" "${BWDIR}"/work/linux-tkg/customization.cfg || true
+    makepkg -s --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}"/linux-tkg/${_qi_lintkg_target} || exit
+    rm -rf "${BWDIR}"/work/ || true
+    mv "${BWDIR}"/work/linux-tkg/*.pkg.tar.zst ~/packages/
+    done < "${BWDIR}/build-scripts/linux-targets"
 }
 
 qern-packages () {
-     sudo pacman -Syu --noconfirm
-     date; time aur sync -uT --no-view --margs --asdeps,--needed,--noconfirm,--noprogressbar,-C,-c,-s,-r "$(cat ${BWDIR}/build-scripts/lists/*/*)" || exit
+    sudo pacman -Syu --noconfirm
+    date; time aur sync -uT --no-view --margs --asdeps,--needed,--noconfirm,--noprogressbar,-C,-c,-s,-r "$(cat ${BWDIR}/build-scripts/lists/*/*)" || exit
 }
 
 nvidia-tkg () {
-     mkdir -p "${BWDIR}"/reuse/{git,cfg/nvidia} || true
-     cd "${BWDIR}"/reuse/git || exit
-     git clone https://github.com/Frogging-Family/nvidia-all || true
-     cd "${BWDIR}"/reuse/git/nvidia-all || exit
-     git fetch --all
-     git reset --hard origin/master
-     rm "${BWDIR}"/reuse/cfg/nvidia/* || true
-     cp "${BWDIR}"/build-scripts/cfg/nvidia-tkg/nvidia.cfg "${BWDIR}"/reuse/cfg/nvidia/
-     sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/nvidia-all.cfg|_EXT_CONFIG_PATH=${BWDIR}/reuse/cfg/nvidia/nvidia.cfg|g" "${BWDIR}"/reuse/git/nvidia-all/customization.cfg || true
-     cd "${BWDIR}"/reuse/git/nvidia-all || exit
-     makepkg -sfCc --noconfirm || exit
-     cp "${BWDIR}"/reuse/git/nvidia-all/*.pkg.tar.zst ~/packages/
+    mkdir -p "${BWDIR}/work/"
+    git clone https://github.com/Frogging-Family/nvidia-all
+    cd "${BWDIR}"/nvidia-all || exit
+    sed -i "s|_EXT_CONFIG_PATH=~/.config/frogminer/nvidia-all.cfg|_EXT_CONFIG_PATH=${BWDIR}/build-scripts/cfg/nvidia/nvidia.cfg|g" "${BWDIR}"/work/nvidia-all/customization.cfg || true
+    cd "${BWDIR}"/work/nvidia-all/
+    makepkg -s --noconfirm
+    mv "${BWDIR}"/work/nvidia-all/*.pkg.tar.zst ~/packages/
 }
 
 proton-tkg () {
-     while IFS= read -r _qi_wine_arg; do
-     export ${_qi_wine_arg}
-     touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/proton-tkg/${_qi_target_nice}"
-     rm -rf "${BWDIR}"/reuse/cfg/proton-tkg/ || true
-     mkdir -p "${BWDIR}"/reuse/{git,/cfg/proton-tkg} || true
-     cd "${BWDIR}"/reuse/git || exit
-     git clone https://github.com/Frogging-Family/wine-tkg-git/ || true
-     cd "${BWDIR}"/reuse/git/wine-tkg-git || exit
-     git fetch --all || exit
-     git reset --hard origin/master || exit
-     sudo pacman -S --noconfirm --needed schedtool || true
-     rm -rf "${BWDIR}/reuse/git/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/{advanced-,}customization.cfg"
-     cp "${BWDIR}"/build-scripts/cfg/wine-tkg/wine.cfg "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/customization.cfg
-     eval sed 's/GCCTUNE/${_qi_target_gcc}/g' "${BWDIR}/build-scripts/cfg/wine-tkg/wine-advanced.cfg" > "${BWDIR}/reuse/git/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/advanced-customization.cfg"
-     rm -rf "${BWDIR}/reuse/git/wine-tkg-git/wine-tkg-git/proton-tkg/proton-tkg-profiles/{advanced-customization,proton-tkg}.cfg"
-     eval sed 's/GCCTUNE/${_qi_target_gcc}/g' "${BWDIR}/build-scripts/cfg/proton-tkg/proton-advanced.cfg" > "${BWDIR}/reuse/git/wine-tkg-git/proton-tkg/proton-tkg-profiles/advanced-customization.cfg"
-     cp "${BWDIR}"/build-scripts/cfg/proton-tkg/proton.cfg "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/proton-tkg.cfg
-     cd "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/ || exit
-     eval sed -i 's/pkgname=proton-tkg-git/pkgname=proton-tkg-${_qi_target_nice}-qern/g' PKGBUILD
-     makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/proton-tkg/${_qi_target_nice}" || exit
-     mv "${BWDIR}"/reuse/git/wine-tkg-git/proton-tkg/*.pkg.tar.zst ~/packages/ || true
-     done < "${BWDIR}/build-scripts/wine-targets"
+    while IFS= read -r _qi_wine_arg; do
+    export ${_qi_wine_arg}
+    touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/proton-tkg/${_qi_target_nice}"
+    rm -rf "${BWDIR}"/work/ || true
+    mkdir -p "${BWDIR}/work/"
+    cd "${BWDIR}/work/"
+    git clone https://github.com/Frogging-Family/wine-tkg-git/ || true
+    cd "${BWDIR}/work/wine-tkg-git/proton-tkg/"
+    eval sed 's/GCCTUNE/${_qi_target_gcc}/g' "${BWDIR}/build-scripts/cfg/wine-tkg/wine-advanced.cfg" > "${BWDIR}/work/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/advanced-customization.cfg"
+    eval sed 's/GCCTUNE/${_qi_target_gcc}/g' "${BWDIR}/build-scripts/cfg/proton-tkg/proton-advanced.cfg" > "${BWDIR}/work/wine-tkg-git/proton-tkg/proton-tkg-profiles/advanced-customization.cfg"
+    cp "${BWDIR}"/build-scripts/cfg/proton-tkg/proton.cfg "${BWDIR}"/work/wine-tkg-git/proton-tkg/proton-tkg.cfg
+    eval sed -i 's/pkgname=proton-tkg-git/pkgname=proton-tkg-${_qi_target_nice}-qern/g' PKGBUILD
+    makepkg -s --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/proton-tkg/${_qi_target_nice}" || exit
+    mv "${BWDIR}"/work/wine-tkg-git/proton-tkg/*.pkg.tar.zst ~/packages/ || true
+    done < "${BWDIR}/build-scripts/wine-targets"
+
 }
 
 wine-tkg () {
-     while IFS= read -r _qi_wine_arg; do
-     export ${_qi_wine_arg}
-     touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/wine-tkg/${_qi_target_nice}"
-     rm -rf "${BWDIR}"/reuse/cfg/wine-tkg/ || true
-     mkdir -p "${BWDIR}"/reuse/{git,/cfg/wine-tkg} || true
-     cd "${BWDIR}"/reuse/git || exit
-     git clone https://github.com/Frogging-Family/wine-tkg-git/ || true
-     cd "${BWDIR}"/reuse/git/wine-tkg-git || exit
-     git fetch --all || exit
-     git reset --hard origin/master || exit
-     sudo pacman -S --noconfirm --needed schedtool || true
-     rm -rf "${BWDIR}/reuse/git/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/{advanced-,}customization.cfg"
-     cp "${BWDIR}"/build-scripts/cfg/wine-tkg/wine.cfg "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/customization.cfg
-     eval sed 's/GCCTUNE/${_qi_target_gcc}/g' "${BWDIR}/build-scripts/cfg/wine-tkg/wine-advanced.cfg" > "${BWDIR}/reuse/git/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/advanced-customization.cfg"
-     cd "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/ || exit
-     makepkg -sfCc --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/wine-tkg/${_qi_target_nice}" || exit
-     mv "${BWDIR}"/reuse/git/wine-tkg-git/wine-tkg-git/*.pkg.tar.zst ~/packages/ || true
-     done < "${BWDIR}/build-scripts/wine-targets"
+    while IFS= read -r _qi_wine_arg; do
+    export ${_qi_wine_arg}
+    touch ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/wine-tkg/${_qi_target_nice}"
+    rm -rf "${BWDIR}"/work/ || true
+    mkdir -p "${BWDIR}/work/"
+    cd "${BWDIR}/work/"
+    git clone https://github.com/Frogging-Family/wine-tkg-git/ || true
+    cd "${BWDIR}/work/wine-tkg-git/wine-tkg-git"
+    cp "${BWDIR}"/build-scripts/cfg/wine-tkg/wine.cfg "${BWDIR}"/work/wine-tkg-git/wine-tkg-git/customization.cfg
+    eval sed 's/GCCTUNE/${_qi_target_gcc}/g' "${BWDIR}/build-scripts/cfg/wine-tkg/wine-advanced.cfg" > "${BWDIR}/work/wine-tkg-git/wine-tkg-git/wine-tkg-profiles/advanced-customization.cfg"
+    makepkg -s --noconfirm 2>&1 | tee -a ~/packages/cronlog/"${_qi_build_year}/${_qi_build_month}/${_qi_build_day}/${_qi_build_time}/wine-tkg/${_qi_target_nice}" || exit
+    mv "${BWDIR}"/work/wine-tkg-git/wine-tkg-git/*.pkg.tar.zst ~/packages/ || true
+    done < "${BWDIR}/build-scripts/wine-targets"
+
 }
 
-
-
-main "$@"; exit
-
+main; exit
