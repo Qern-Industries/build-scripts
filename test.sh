97d0b071cc354cdb45f4b5d81d913ef58f3a2382
Orangian <tysayahi@gmail.com>
2022-05-31 09:51:17 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,22 @@
 set -e
+echo $?
 set -x
+echo $?
+
       cd ..
+      echo $?
+
      BWDIR=$(pwd)
+echo $?
 
      while IFS= read -r _qi_linux_arg; do
+     echo $?
+
      export ${_qi_linux_arg}
+     echo $?
+
      echo ${_qi_linux_arg}
+echo $?
+
      done < "${BWDIR}/build-scripts/linux-targets"
+echo $?
